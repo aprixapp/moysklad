e172d2fea57b96e75d5e982c768432b53d6db5cd
Ilya <user6000@aprix.ru>
2024-06-04 10:46:54 +0300

    Add getListCustomTemplate to EntityWithPublicationAndTemplate.php

--- src/Traits/EntityWithPublicationAndTemplate.php
+++ src/Traits/EntityWithPublicationAndTemplate.php
@@ -2,6 +2,7 @@
 
 namespace AprixApp\MoySklad\Traits;
 
+use AprixApp\MoySklad\MSConnect;
 use AprixApp\MoySklad\MSTools;
 
 trait EntityWithPublicationAndTemplate
@@ -20,5 +21,12 @@ trait EntityWithPublicationAndTemplate
             ->setBody($arBodyResponse, true)
             ->modified()
             ->getJsonResponse();
+    }
+
+    public function getListCustomTemplates(): MSConnect
+    {
+        $partHref = static::ELEMENT_PART_HREF . '/' . static::CODE_ENTITY . '/metadata/customtemplate';
+
+        return $this->connect->get($partHref);
     }
 }
