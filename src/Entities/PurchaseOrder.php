4e7e5ed33c5b2c559424c9280fda5c5caa4fd005
Ilya <user6000@aprix.ru>
2024-11-13 13:14:58 +0300

    add entitywithmetadata trait to purchase order

--- src/Entities/PurchaseOrder.php
+++ src/Entities/PurchaseOrder.php
@@ -3,8 +3,11 @@
 namespace AprixApp\MoySklad\Entities;
 
 use AprixApp\MoySklad\MSEntity;
+use AprixApp\MoySklad\Traits\EntityWithMetadata;
 
 class PurchaseOrder extends MSEntity
 {
+    use EntityWithMetadata;
+
     const CODE_ENTITY = "purchaseorder";
 }
